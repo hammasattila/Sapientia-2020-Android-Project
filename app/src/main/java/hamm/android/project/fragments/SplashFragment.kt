a278f860780795285713fe353ef283a608cf96f1
Attila Hammas <hammasattila@outlook.com>
2020-12-03 19:03:08 +0200

    Added Splash Screen.

---
+++ app/src/main/java/hamm/android/project/fragments/SplashFragment.kt
@@ -0,0 +1,20 @@
+package hamm.android.project.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import hamm.android.project.R
+
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+}
